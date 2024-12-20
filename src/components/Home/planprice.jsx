8271b9ea32061f297e12452f994833f12442543c
unknown <asolkarprutvhi7@gmail.com>
2024-12-20 15:21:54 +0530

    image fix5

--- src/components/Home/planprice.jsx
+++ src/components/Home/planprice.jsx
@@ -13,7 +13,15 @@ const Planprice = () => {
             <div><h2>Are you an <span>Agency?</span></h2></div>
             <div className='agencysubtxt'><img  className="rocketimg"src="/assets/Group 24063.svg" alt="" /><p>Then This plan fits you <span>perfectly!</span></p><img  className="shineimg"src="/assets/shines (1).svg" alt="" /></div>
             </div>
-            <ToggleSwitch/>
+            <div className='switchtoggle'><ToggleSwitch/></div>
+
+
+            <div className="planbtns">
+                        <div><button className='btnplan1 active'>SEO</button></div>
+                        <div><button className='btnplan2 '>Social and ads  </button></div>
+                        <div><button className='btnplan3'>Digital</button></div>
+                    </div>
+            
         </div>
     )
 }
