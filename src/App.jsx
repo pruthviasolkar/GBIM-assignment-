4f9af32b36daa180fea133746b0413e5464356ba
unknown <asolkarprutvhi7@gmail.com>
2024-12-20 16:51:55 +0530

    image fix5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+ 
 import viteLogo from '/vite.svg'
 import Navbar from './components/navbar/Navbar'
 import './App.css'
--- src/components/Home/planprice.jsx
+++ src/components/Home/planprice.jsx
@@ -6,22 +6,98 @@ const Planprice = () => {
         <div>
             <div className='headplan'>
                 <div><h2>Plans & Pricing</h2></div>
-                <div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut <br/>
+                <div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut <br />
                     labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p></div>
             </div>
             <div className='headagency'>
-            <div><h2>Are you an <span>Agency?</span></h2></div>
-            <div className='agencysubtxt'><img  className="rocketimg"src="/assets/Group 24063.svg" alt="" /><p>Then This plan fits you <span>perfectly!</span></p><img  className="shineimg"src="/assets/shines (1).svg" alt="" /></div>
+                <div><h2>Are you an <span>Agency?</span></h2></div>
+                <div className='agencysubtxt'><img className="rocketimg" src="/assets/Group 24063.svg" alt="" /><p>Then This plan fits you <span>perfectly!</span></p><img className="shineimg" src="/assets/shines (1).svg" alt="" /></div>
             </div>
-            <div className='switchtoggle'><ToggleSwitch/></div>
+            <div className='switchtoggle'><ToggleSwitch /></div>
 
 
             <div className="planbtns">
-                        <div><button className='btnplan1 active'>SEO</button></div>
-                        <div><button className='btnplan2 '>Social and ads  </button></div>
-                        <div><button className='btnplan3'>Digital</button></div>
+                <div><button className='btnplan1 active'>SEO</button></div>
+                <div><button className='btnplan2 '>Social and ads  </button></div>
+                <div><button className='btnplan3'>Digital</button></div>
+            </div>
+
+
+            <div className='planlist'>
+                <div className="planlistcard"><div className='headplancard'><h3>Free Digital</h3><p>For Professional Use</p></div>
+                    <div className='descplancard'>
+                        <ul>
+                            <li><img src="/assets/check.svg" alt="" /><p>For Small Agencies</p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>Agency Dashboard
+                            </p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>Manage up to 50 Projects</p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>Track 5,000 Keywords</p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>5 Team Members</p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>Competitor Analysis, On <br />
+                                Page / Website Audit, Backlink <br />
+                                Checker and Monitor, <br />
+                                Analytics & Reporting with <br />
+                                more features.</p></li>
+                        </ul>
                     </div>
-            
+                    <div className='btnplancard'><button>choose plan</button></div>
+                </div>
+                <div className="planlistcard active"><div className='headplancard1 '><h3>Premium Economy</h3><p>For Professional Use</p></div>
+                    <div className='descplancard'>
+                        <ul>
+                            <li><img className='descimg'  src="/assets/check.svg" alt="" /><p>For Small Agencies</p></li>
+                            <li><img className='descimg' src="/assets/check.svg" alt="" /><p>Agency Dashboard
+                            </p></li>
+                            <li><img className='descimg' src="/assets/check.svg" alt="" /><p>Manage up to 50 Projects</p></li>
+                            <li><img className='descimg' src="/assets/check.svg" alt="" /><p>Track 5,000 Keywords</p></li>
+                            <li><img className='descimg' src="/assets/check.svg" alt="" /><p>5 Team Members</p></li>
+                            <li><img className='descimg' src="/assets/check.svg" alt="" /><p>Competitor Analysis, On <br />
+                                Page / Website Audit, Backlink <br />
+                                Checker and Monitor, <br />
+                                Analytics & Reporting with <br />
+                                more features.</p></li>
+                        </ul>
+                    </div>
+                    <div className='btnplancard1'><button>choose plan</button></div>
+                </div>
+                <div className="planlistcard thirdcard"> <div className="imgpopular"><img className='mostpopular' src="/assets/Mostpopular.svg" alt="" /></div><div className='headplancard'><h3>Premium Business</h3><p>For Professional Use</p></div>
+                    <div className='descplancard'>
+                        <ul>
+                            <li><img src="/assets/check.svg" alt="" /><p>For Small Agencies</p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>Agency Dashboard
+                            </p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>Manage up to 50 Projects</p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>Track 5,000 Keywords</p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>5 Team Members</p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>Competitor Analysis, On <br />
+                                Page / Website Audit, Backlink <br />
+                                Checker and Monitor, <br />
+                                Analytics & Reporting with <br />
+                                more features.</p></li>
+                        </ul>
+                    </div>
+                    <div className='btnplancard'><button>choose plan</button></div>
+                </div>
+                <div className="planlistcard"><div className='headplancard'><h3>Premium First</h3><p>For Professional Use</p></div>
+                    <div className='descplancard'>
+                        <ul>
+                            <li><img src="/assets/check.svg" alt="" /><p>For Small Agencies</p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>Agency Dashboard
+                            </p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>Manage up to 50 Projects</p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>Track 5,000 Keywords</p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>5 Team Members</p></li>
+                            <li><img src="/assets/check.svg" alt="" /><p>Competitor Analysis, On <br />
+                                Page / Website Audit, Backlink <br />
+                                Checker and Monitor, <br />
+                                Analytics & Reporting with <br />
+                                more features.</p></li>
+                        </ul>
+                    </div>
+                    <div className='btnplancard'><button>choose plan</button></div>
+                </div>
+            </div>
+
         </div>
     )
 }
