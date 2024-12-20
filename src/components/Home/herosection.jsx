3c1a97c72c26f2505bd1142cbd3bc3ebe20a876a
unknown <asolkarprutvhi7@gmail.com>
2024-12-20 14:49:49 +0530

    image fix4

--- src/components/Home/herosection.jsx
+++ src/components/Home/herosection.jsx
@@ -8,17 +8,17 @@ const Herosection = () => {
         <div className='contactform'><div className="maincontactform"><div className='formhead'><h3>Contact Us To <span>Grow!</span>
         </h3><img src="/assets/growth-chart.svg" alt="" /></div>
           <div className="formbody">
-          <input type="text" placeholder='Full Name' />
-          <input type="text" placeholder='Phone Number' />
-          <input type="text"  placeholder='Email Address'/>
-          <textarea placeholder="Message" rows="4" cols="50"></textarea>
-          <label class="checkbox-button">
-            <input type="checkbox" />
-            <span className='checkboxspan'>I agree to all <span className='tc'>terms & conditions</span></span>
-          </label>
-          <button className='formbtn'>SEND <img src="/assets/arrowright.png" alt="" /></button>
+            <input type="text" placeholder='Full Name' />
+            <input type="text" placeholder='Phone Number' />
+            <input type="text" placeholder='Email Address' />
+            <textarea placeholder="Message" rows="4" cols="50"></textarea>
+            <label class="checkbox-button">
+              <input type="checkbox" />
+              <span className='checkboxspan'>I agree to all <span className='tc'>terms & conditions</span></span>
+            </label>
+            <button className='formbtn'>SEND <img src="/assets/arrowright.png" alt="" /></button>
           </div>
-          
+
         </div>
         </div>
       </div>
